9cb76e9ae3e265fcc22626cf659665e0ac67832e
ShalindaAmil <shalindaamil22@gmail.com>
2025-05-09 14:16:49 +0530

    finishing pages in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes,Route,Navigate}from 'react-router-dom';
+import {AuthProvider,useAuth}from './contexts/authcontext';
+import LogingPage from '.pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import TaskListPage from './pages/TaskListPage';
+import AddTaskPage from './pages/AddTaskPage';
+import EditTaskPage from './pages/EditTaskPage';
+import Navbar from './components/Navbar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App(){
+  return(
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-100"> 
+          <Navbar/>
+          <div className="container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="/login" element={<LogingPage/>} />
+              <Route path="/dashboard" element={<DashboardPage/>} />
+              <Route path="/tasks" element={<TaskListPage/>} />
+              <Route path="/tasks/add" element={<AddTaskPage/>} />
+              <Route path="/tasks/edit/:id" element={<EditTaskPage/>} />
+            </Routes>
+           </div> 
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+const ProtectedRoute = ({children}) => {
+  const{user, loading}=useAuth();
+  if(loading){
+    return <div className='text-center py-10'>Loading...</div>;
+  }
+  if(!user){
+    return <Navigate to="/login" replace/>;
+  }
+  return children;
+};
+
+export default App;
